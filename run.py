68a6e895c49d7565fcc0a2ce798c371c38d36a2c
Safi Hasan <safihasan@gmail.com>
2024-06-19 10:09:20 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -13,31 +10,31 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
-    while True:     
-      print("Please enter sales data from the last market.")
-      print("Data should be six numbers, separated by commas.")
-      print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-      data_str = input("Enter your data here:")
-    
-      sales_data = data_str.split(",")
-     
-      if validate_data(sales_data):
-        print("Data is valid!")
-        break
-return sales_data
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -45,7 +42,6 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
-    print(values)
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -54,7 +50,21 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        return False        
-    
-    return True 
-data = get_sales_data()    
+        return False
+
+    return True
+
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
